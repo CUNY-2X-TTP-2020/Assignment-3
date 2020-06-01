8eb33894fa0fe5c32045a23d658f6d3a3152da8b
RickyC0626 <rickychon99@gmail.com>
2020-06-01 17:45:10 -0400

    Add console logs with output in comments

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 // 1 - Select the section with an id of container without using querySelector
 let container = document.getElementById("container");
+console.log(container); // <section id="container">...</section>
 
 // 2 - Select the section with an id of container using querySelector
 let querySelected = document.querySelector("section");
+console.log(querySelected); // <section id="container">...</section>
 
 // 3 - Select all of the list items with a class of "second"
 let second = document.getElementsByClassName("second");
+console.log(second); // [li.second, li.second]
 
 // 4 - Select a list item with a class of third, but only the list item inside of the ol tag
 let item = document.getElementsByClassName("third")[1];
-console.log(item);
+console.log(item); // <li class="third">three</li>
 
 // 5 - Give the section with an id of container the text "Hello!"
 let section = document.getElementById("container");
 section.innerText = "Hello";
-
+console.log(section); // <section id="container">...</section>
 
 // 6 - Add the class main to the div with a class of footer
 document.getElementsByClassName("footer")[0].classList.add("main");
-
+// <div class="footer main"></div>
 
 // 7 - Remove the class main on the div with a class of footer
 
