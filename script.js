08ca2b99a11118057364476d6bf542631d4acb2e
Omaru Jawara <omaru.jawara94@gmail.com>
2020-06-01 15:24:02 -0400

    Removed the class main on the div with a class of footer

--- script.js
+++ script.js
@@ -1,47 +1,24 @@
 // 1 - Select the section with an id of container without using querySelector
 
-
-
 // 2 - Select the section with an id of container using querySelector
-
-
 
 // 3 - Select all of the list items with a class of "second"
 
-
-
 // 4 - Select a list item with a class of third, but only the list item inside of the ol tag
-
-
 
 // 5 - Give the section with an id of container the text "Hello!"
 
-
-
 // 6 - Add the class main to the div with a class of footer
 
-
-
 // 7 - Remove the class main on the div with a class of footer
-
-
+document.querySelector("div .footer .main").className = "main";
 
 // 8 - Create a new li element
-
-
-
+let newLiElement = document.createElement('li');
 // 9 - Give the li the text "four"
-
-
-
+newLiElement.innerHTML = 'four'
 // 10 - Append the li to the ul element
-
-
 
 // 11 - Loop over all of the lis inside the ol tag and give them a background color of "green"
 
-
-
 // 12 - Remove the div with a class of footer
-
-
